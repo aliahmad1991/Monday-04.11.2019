fa2a4baafa48c5773481dbb72d6d28a3b4179ee4
aliahmad1991 <aliahmadheydarzadeh@gmail.com>
2019-11-04 14:21:55 +0100

    Updates

---
+++ precache-manifest.01a003baa3fb378633337c23640b59d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38740774a59e3975b45fac1394989957",
+    "url": "/Monday-04.11.2019/index.html"
+  },
+  {
+    "revision": "35a25a7dce6f12f1c753",
+    "url": "/Monday-04.11.2019/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9c5e349caa8c0dcb13e6",
+    "url": "/Monday-04.11.2019/static/js/2.ea0fe5d6.chunk.js"
+  },
+  {
+    "revision": "35a25a7dce6f12f1c753",
+    "url": "/Monday-04.11.2019/static/js/main.fa73f2d7.chunk.js"
+  },
+  {
+    "revision": "632bfd01454c8bfeaea6",
+    "url": "/Monday-04.11.2019/static/js/runtime-main.bd634505.js"
+  }
+]);
